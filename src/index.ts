ce136df67e72cd636382f857600268d42764e39b
Dawid Hermann <dawid.hermann_ext@scout24.com>
2025-12-26 20:31:11 +0100

    Refactor index.ts for consistent export syntax
    - Updated export statements in index.ts to use double quotes for consistency across the file.
    - Removed unnecessary line breaks to enhance readability and maintainability of the code structure.

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,18 @@
 // Main exports
-export { default as RequestChain, begin } from './core/RequestChain';
-export { default } from './core/RequestChain';
+export { default as RequestChain, begin } from "./core/RequestChain";
+export { default } from "./core/RequestChain";
 
 // Adapters
-export { default as RequestAdapter } from './core/RequestAdapter';
-export { default as FetchRequestAdapter } from './core/adapters/FetchRequestAdapter';
+export { default as RequestAdapter } from "./core/RequestAdapter";
+export { default as FetchRequestAdapter } from "./core/adapters/FetchRequestAdapter";
 
 // Types
 export type {
   IRequestConfig,
   IRequestConfigFactory,
-  IRequestResult,
   PipelineRequestStage,
   PipelineManagerStage,
   BasePipelineStage,
-} from './core/models/RequestParams';
+} from "./core/models/RequestParams";
 
-export type {
-  ErrorHandler,
-  ResultHandler,
-} from './core/models/Handlers';
-
+export type { ErrorHandler, ResultHandler } from "./core/models/Handlers";
